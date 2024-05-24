feb6bed8ea9e675224c38ec35d48f3e01f0107b2
EniuK <eniuk113@gmail.com>
2024-05-24 13:34:35 +0200

    add searchbar popup

---
+++ src/components/maincardComponents/TagPopup.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import { useMyContext } from "../../context/Context.tsx";
+
+interface Tag {
+  tagName: string;
+  text: string;
+  numberOfSelectedTags: number;
+}
+
+const TagList: React.FC = () => {
+  const { tags, items, setItems } = useMyContext();
+
+  const [actualState, setActualState] = useState<Tag[]>(items);
+
+  function checkState(name: string): boolean {
+    return actualState.some((el) => name === el.tagName);
+  }
+
+  function handleChange(
+    event: React.ChangeEvent<HTMLInputElement>,
+    tag: Tag
+  ): void {
+    const { checked } = event.target;
+    const help = actualState;
+    if (checked) {
+      setActualState((prev) => [...prev, tag]);
+    } else {
+      const updatedHelp = help.filter((e) => e.tagName !== tag.tagName);
+      setActualState(updatedHelp);
+    }
+  }
+
+  function handleAddSelectedTags(): void {
+    setItems(actualState);
+  }
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      {tags.map((tag) => (
+        <div
+          key={tag.tagName}
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <div>
+            <input
+              type="checkbox"
+              name={tag.text}
+              id={tag.tagName}
+              checked={checkState(tag.tagName)}
+              onChange={(event) => handleChange(event, tag)}
+            />
+            {tag.text}
+          </div>
+          <div>{tag.numberOfSelectedTags}</div>
+        </div>
+      ))}
+      <div>
+        <button
+          onClick={handleAddSelectedTags}
+          style={{ textAlign: "center", width: "100%", marginTop: "10px" }}
+        >
+          Zapisz
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TagList;
