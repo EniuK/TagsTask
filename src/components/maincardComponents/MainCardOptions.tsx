89b3cff5565d652eacc6e6d33597afe67d3cba08
EniuK <eniuk113@gmail.com>
2024-05-24 13:49:38 +0200

    add changes to tag display

--- src/components/maincardComponents/MainCardOptions.tsx
+++ src/components/maincardComponents/MainCardOptions.tsx
@@ -3,20 +3,24 @@ import "./maincardOptions.css";
 interface Option {
   source: string;
   text: string;
+  disabled: boolean;
 }
 
 const starsOptions: Option[] = [
   {
     source: "./public/options/starscolored.svg",
     text: "CMS AI",
+    disabled: true,
   },
   {
     source: "./public/options/stars.svg",
     text: "Analizuj tekst",
+    disabled: false,
   },
   {
     source: "./public/options/tagcolored.svg",
     text: "Najbardziej popularne tagi",
+    disabled: false,
   },
 ];
 
@@ -30,7 +34,12 @@ const MainCardOptions: React.FC = () => {
           className="maincard-options-map-container"
         >
           <img src={option.source} alt="icon" width={"40px"} height={"40px"} />
-          <span className="maincard-options-map-span">{option.text}</span>
+          <span
+            className="maincard-options-map-span"
+            style={option.disabled ? { color: "gray" } : {}}
+          >
+            {option.text}
+          </span>
         </div>
       ))}
     </>
